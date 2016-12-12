e0e103a06d8954d96904c66927dfc2d2953a56fa
CodeJCSON <yunmei@yunmeideiMac.local>
2016-12-12 14:39:46 +0800

    SGScanningQRCodeVC
    扫描二维码框架

---
+++ 扫描二维码/Controller/SGScan.h
@@ -0,0 +1,17 @@
+//
+//  SGScan.h
+//  xunmaobianlidian
+//
+//  Created by 周宾 on 16/11/2.
+//  Copyright © 2016年 周宾. All rights reserved.
+//
+
+#ifndef SGScan_h
+#define SGScan_h
+
+#import "SGGenerateQRCodeVC.h"
+#import "SGScanningQRCodeVC.h"
+#import <AVFoundation/AVFoundation.h>
+#import "SGAlertView.h"
+
+#endif /* SGScan_h */
